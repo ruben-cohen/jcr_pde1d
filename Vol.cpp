61b77ad278ce86e4528e00c9ea030c41216e6251
JulienBaudin <julien.g.baudin@gmail.com>
2020-01-22 19:26:45 +0100

    Volatility class

---
+++ Vol.cpp
@@ -0,0 +1,68 @@
+
+//Cpp
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Classe de base
+	//Constructor
+	volatility::volatility(const double& v,const mesh& grid)
+	:
+	 m_init_vol(v),
+	 m_grid(grid)
+	{
+		m_vector_time = m_grid.get_;
+		m_vector_stock = m_grid.get;
+		m_nb_time = m_vector_time.size();
+		m_nb_spot = m_vector_stock.size();	
+	};
+	
+	//Here no computation since vol is constant --> just return init vol 
+	double volatility::compute_vol(const double& S,const double& t,const double& x = 0.,const double& y = 0.)
+	{
+		return m_init_vol;
+	};
+	
+	std::vector<double> volatility::vector_vol(const std::vecteur<double> v_spot,const double& t)
+	{
+		std::size_t nb_step = v_spot.size();
+		
+		//m_vol_const.resize(nb_step,0.);
+		
+		for (long i = 0; i < nb_step ; ++i)
+		{
+			m_vol_const.push_back(compute_vol());
+		}
+		
+		return m_vol_const;
+	};
+//////////////////////////////////////////////////////////////////////////////
+Classe qui hérite pour la surface de vol
+
+	vol_surface::vol_surface(const double& v, mesh grid,const double& coeff_tps, const double& coeff_spot)
+	:
+	 volatility(v, grid), //Normalament on obtient ainsi les variables protected de volatility
+	 m_coeff_spot(coeff_spot),
+	 m_coeff_time(coeff_tps)
+	{
+	};
+	
+	//Here computation of a vol thanks to parameters
+	double vol_surface::compute_vol(const double& S,const double& t,const double& x,const double& y)
+	{
+		double vol = x*t + y*S;
+		
+		return vol;
+	};
+	
+	std::vector<double> vol_surface::vector_vol(const std::vecteur<double> v_spot,const double& t)
+	{
+		std::size_t nb_step = v_spot.size();
+		
+		//m_vol_const.resize(nb_step,0.);
+		
+		for (long i = 0; i < nb_step ; ++i)
+		{
+			m_vol_matrix.push_back(compute_vol(v_spot(i),t,m_coeff_time,m_coeff_spot));
+		}
+		
+		return m_vol_matrix;
+	};	
+
---
+++ vol.hpp
@@ -0,0 +1,51 @@
+//Partie hpp	
+	//Classe de base, permet aussi le cas le plus simple ou la vol est constante
+	class volatility 
+	{
+	
+	public: 
+		//Constructor
+		volatility(const double& v,const mesh& grid); //Constructor, base class only takes initial value as input
+		
+		//Method to compute a vol (vol const donc les deux coeffs valent zéro)
+		double compute_vol(const double& S = 0,const double& t = 0,const double& x = 0.,const double& y = 0.); //To compute a vol
+		//Method to compute vector or vol (for one step of time)
+		std::vector<double> vector_vol(const std::vecteur<double> v_spot,const double& t);
+		
+	private:
+	
+		std::vector<double> m_vol_const;
+
+	
+	protected:
+		
+		std::vector<double> m_vector_time;
+		std::vector<double> m_vector_stock;
+		std:size_t m_nb_time;
+		std:size_t m_nb_spot;	
+		double m_init_vol;
+		mesh m_grid;
+		
+	};
+	 //Classe ou on va dire que la volatility est fonction du temps et du spot
+	//On pourra créer ainsi d'autres classes avec différents comportement pour la vol (e.g. seulement une fonction du temps)
+	class vol_surface : public volatility
+	{
+	public:
+	
+		//Niveau paramètre, la classe prends en plus les deux coeff pour définit la fonction
+		vol_surface(const double& v, mesh grid, const double& coeff_tps, const double& coeff_spot);
+		
+		//On reprends les mêmes méthodes (du coup la les coeffs ne sont plus constant zéro comme au dessus)
+		double compute_vol(const double& S,const double& t,const double& x,const double& y); 
+		std::vector<double> vector_vol(const std::vecteur<double> v_spot,const double& t);
+
+		
+	private:
+	
+		double m_coeff_time;
+		double m_coeff_spot;
+		std::vector<double> m_vol_matrix;
+		
+	
+	};
