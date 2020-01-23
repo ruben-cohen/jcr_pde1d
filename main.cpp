22e7eb2cf77ca8c3183b082cfe394a0bf225c9e6
JulienBaudin <julien.g.baudin@gmail.com>
2020-01-23 02:09:58 +0100

    Debug partiel et merge des parties

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 // #include "xtensor/xadapt.hpp"
 // #include "xtensor/xeval.hpp"
 // // Allows to perform linear algebra operations on xarrays
-#include "xtensor-blas/xlinalg.hpp"
+// #include "xtensor-blas/xlinalg.hpp"
 
 
 ////
@@ -28,7 +28,7 @@ int main(int argc, char* argv[])
 	long nb_step_spot =20;    // Spot goes from [0.0, 1.0]
 	long nb_step_time = 20; 
 	
-	xt::xarray<double> diago = xt::eye(20,-1);
+	
 	// Create the PayOff object (strike as parameter)
 	PayOff* option = new PayOffCall(K);
 	// Create the mesh object (parameters (S,Vol, Nb time step, nb stock step)
@@ -38,17 +38,25 @@ int main(int argc, char* argv[])
 	//PDE m(option,grille.getdx(),grille.getdt(),grille.Getvector_time(),grille.Getvector_stock());
 	
 	//test of the boundaries : 
-	Parameters par(v, r, theta_);
+	//Parameters par(v, r, theta_);
 	//std::vector<double> K_v{0.1,0.1,0.1,0.1};
 	//std::vector<double> K_v2{0.1,0.1,0.1,0.1};
-	Derichtlet c(grille,par);
+	//Derichtlet c(grille,par);
 	//Neumann c2(grille,par,K_v);
 	//print(K_v);
-	xt::xtensor<double, 1> K_v = {0.1, 0.1, 0.1, 0.1 };
-	xt::xtensor<double, 1> K_v2 = {0.1, 0.1, 0.1, 0.1 };
-	xt::xarray<double> res = xt::linalg::dot(K_v, K_v2);
+	
+	//std::vector<double> m_data(2 * 3);
+	
+	//m_data
+	// m_data(1
+	
+	
+	//xt::xarray<double> diago = xt::eye(20,-1);
+	// xt::xtensor<double, 1> K_v = {0.1, 0.1, 0.1, 0.1 };
+	// xt::xtensor<double, 1> K_v2 = {0.1, 0.1, 0.1, 0.1 };
+	// xt::xarray<double> res = xt::linalg::dot(K_v, K_v2);
 	std::vector<double> der = c.get_cond();
-	std::cout<< res()<< std::endl;
+	// std::cout<< res()<< std::endl;
 	//xt::xarray<double> res = xt::linalg::dot(K_v, K_v2);
 	
 	//Solve sv(grille, par, der);
@@ -56,23 +64,40 @@ int main(int argc, char* argv[])
 	//xt::xarray<double> price = sv.Get_FX_n();
 	
 	//price.reshape({grille.Getvector_stock().size(),1});
-	std::cout<< diago << std::endl;
-	std::cout<< "solver" << std::endl;
+	//std::cout<< diago << std::endl;
+	//std::cout<< "solver" << std::endl;
 	//std::cout << price << std::endl;
+	
 	std::cout<< "fonction calcul cond init:" << std::endl;
 	std::cout << grille.init_cond(S) << std::endl;
+
 	std::cout<< "Vecteur de prix (log):" << std::endl;
 	print(grille.Getvector_stock());
+	std::cout<< "Size vecteur stock:" << std::endl;
+	std::cout<< grille.Getvector_stock().size() <<std::endl;
+	
 	std::cout<< "Vecteur de temps:" << std::endl;
 	print(grille.Getvector_time());
-	std::cout<< "Vecteur cond init (log):" << std::endl;
+	std::cout<< "Size vecteur temps:" << std::endl;
+	std::cout<< grille.Getvector_time().size() <<std::endl;
+	
+	std::cout<< "Vecteur cond init (not log):" << std::endl;
 	print(grille.get_init_vector());
+	std::cout<< "Size vecteur cond ini:" << std::endl;
+	std::cout<< grille.get_init_vector().size() <<std::endl;
+	
 	std::cout<< "dx:" << std::endl;
 	std::cout << grille.getdx() << std::endl;
+	
 	std::cout<< "dt:" << std::endl;
 	std::cout << grille.getdt() << std::endl;
+	
 	std::cout<< "vecteur dirichlet:" << std::endl;
 	print(c.get_cond());
+	std::cout<< "Size vecteur dirichlet:" << std::endl;
+	std::cout<< c.get_cond().size() <<std::endl;
+	
+	
 	// std::cout<< "vecteur Neumann:" << std::endl;
 	// print(c2.get_cond());
 
